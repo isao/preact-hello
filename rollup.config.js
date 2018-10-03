a22bd262ea2299f9cb68b116962e6df8426fec1b
Isao Yagi <isao.yagi@gmail.com>
2018-10-03 15:51:13 -0700

    manualChunks: {     vendor: ['preact'], }

--- rollup.config.js
+++ rollup.config.js
@@ -2,14 +2,19 @@ import typescript from 'rollup-plugin-typescript'
 import resolve from 'rollup-plugin-node-resolve'
 
 export default {
-  input: './src/app.tsx',
-  output: {
-      format: 'esm',
-      file: 'dist/bundle.js', // Should match package.json module field.
-  },
-  plugins: [
-    typescript(),
-    resolve(),
-  ],
-  //external: ['preact'],
+    input: './src/app.tsx',
+    output: {
+        format: 'esm',
+        file: 'dist/bundle.js',
+    },
+    experimentalCodeSplitting: true,
+    manualChunks: {
+        vendor: ['preact'],
+    },
+    plugins: [
+        typescript(),
+        resolve(),
+    ],
 }
+
+// broken, see https://github.com/rollup/rollup-plugin-node-resolve/issues/164
